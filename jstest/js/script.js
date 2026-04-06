7adba14701a5d45bbea61dd4bcf121bd913f7292
Krishita <krl105@scarletmail.rutgers.edu>
2026-04-06 17:24:13 -0400

    js

---
+++ jstest/js/script.js
@@ -0,0 +1,10 @@
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + "," + g + "," + b + ")";
+  }
+  
+  document.addEventListener("click", function() {
+    document.body.style.backgroundColor = getRandomColor();
+  });
